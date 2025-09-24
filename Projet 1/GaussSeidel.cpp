493b78ed77ffb793fc59ccf2ab9f32a77afb77f4
Clement <clement.fontan@outlook.com>
2025-09-24 11:07:13 +0200

    changed var

--- Projet 1/GaussSeidel.cpp
+++ Projet 1/GaussSeidel.cpp
@@ -11,14 +11,13 @@
 
 //  var global
 
-int n =100;                     // taille matrices
-int Nt =100, Nx= 100;           // nb time step,  nb space step
-double Length_x =1;             // dimension carre
-double Time =1;                 // Temps d arret
-double dx = Length_x/Nx;        // space step
-double dt = Time/Nt;            // time step
+int n;                     // taille matrices
+int Nt,Nx;           // nb time step,  nb space step
+double a,b;             // dimension carre
+double Time;                 // Temps d arret
+double dx;        // space step
+double dt;            // time step
 
-double Length_y =Length_x;
 
 using namespace std;
 
@@ -30,12 +29,31 @@ double f(double x, double y)
 
 double U0(double x, double y)   // gaussian centrer en a/2, b/2
 {
-  double r2 = (x-0.5*Length_x)*(x-0.5*Length_x) + (y-0.5*Length_y)*(y-0.5*Length_y); // carré du rayon au centre du carre
+  double r2 = (x-0.5*a)*(x-0.5*a) + (y-0.5*b)*(y-0.5*b); // carré du rayon au centre du carre
   double a =100;    // parametre de centrage de la gaussienne
   return exp(-a*r2);
 }
 
 int main(int argc, char* argv[]){
+
+    // Paramètres du problème
+    if (argc != 4) {
+        cout << "Il faut entrer 3 arguments, ici il y en a " << argc - 1 << endl;
+        return 1;
+    }
+
+    int Nx = atoi(argv[1]);
+    double a = atoi(argv[2]);
+    double b = atoi(argv[3]);
+    double dx = (b-a)/(Nx+1);
+
+    if (a <= 0) {
+        cout << "a doit être strictement positif!" << endl;
+        return 1;
+    }
+    else if (b <= 0) {
+        cout << "b doit être strictement positif!" << endl;
+    }
 
     vector<double> U (Nx*Nx);     // vecteur de la solution
     
@@ -46,7 +64,7 @@ int main(int argc, char* argv[]){
         
         for(int j;j<Nx;j++)
         {
-            U[i*Nx+j] = U0()
+            U[i*Nx+j];
         } 
     }
 
